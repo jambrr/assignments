bcccab60659950ea9e3af510876749a2d00435b9
jambrr <louis.weber99@gmail.com>
2021-11-01 20:38:28 +0100

    finsihed cloud based

--- cloud-based/docker-nginx-nodejs-mysql/node.js/index.js
+++ cloud-based/docker-nginx-nodejs-mysql/node.js/index.js
@@ -6,11 +6,11 @@ const fs = require('fs');
 const csvtojson = require('csvtojson');
 const https = require('https');
 app.use(bodyParser.json());
-let results;
 
 const client = redis.createClient({host: 'redis'});
 
 app.get('/:country', (req, res) => {
+    console.log(__filename);
 	let country = req.params.country;
     //client.flushdb( function (err, succeeded) {
     //   console.log(succeeded); // will be true if successfull
@@ -19,41 +19,41 @@ app.get('/:country', (req, res) => {
     client.get(country, function(err, rep){
         if(rep != null){
             console.log('Retrieve data from the database');
-            let object = JSON.parse(rep);
-            res.send(object);
+            let jsonObject = JSON.parse(rep);
+            res.send(jsonObject);
         }else{
             console.log('Retrieve data from the website');
             //Download csv file
-            const file = fs.createWriteStream('data.csv');
-            const request = new Promise((resolve, reject) => {
+            const file = fs.createWriteStream(__dirname+'/covid_data2.csv');
+            const httpsRequest = new Promise((resolve, reject) => {
                 https.get('https://covid19.who.int/WHO-COVID-19-global-table-data.csv', function(response){
                   resolve(response.pipe(file));
               });
             });
             
             //Filter the csv file
-            request.then(value => {
-                csvtojson().fromFile('data.csv')
+            httpsRequest.then(value => {
+                csvtojson().fromFile('covid_data.csv')
                 .then(data => {
-                    for(var x in data){
-                        if(data[x].Name === country){
+                    for(var i in data){
+                        if(data[i].Name === country){
 
-                            let name = data[x]['Name'];
-                            let cases_7_days = data[x]['Cases - newly reported in last 7 days'];
-                            let cases_24_hrs = data[x]['Cases - newly reported in last 24 hours'];
+                            let name = data[i]['Name'];
+                            let cases_7_days = data[i]['Cases - newly reported in last 7 days'];
+                            let cases_24_hrs = data[i]['Cases - newly reported in last 24 hours'];
 
                             let date = new Date();
 
-                            let object = {
+                            let jsonObject = {
                                 'country': name,
                                 'date': date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate(),
                                 'cases_7_days': cases_7_days,
                                 'cases_24_hrs': cases_24_hrs
                             };
                             
-                            results = object;
-                            client.set(country, JSON.stringify(object));
-                            res.send(object);
+                            results = jsonObject;
+                            client.set(country, JSON.stringify(jsonObject));
+                            res.send(jsonObject);
                         }
                     }
                 }).catch(err => {
