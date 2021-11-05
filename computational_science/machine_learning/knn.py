097d0fed154d1310106db0d80a2280e3151aaeaa
jambrr <louis.weber99@gmail.com>
2021-11-06 00:08:09 +0100

    knn

--- computational_science/machine_learning/knn.py
+++ computational_science/machine_learning/knn.py
@@ -1,5 +1,5 @@
 import pandas
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 
@@ -11,12 +11,13 @@ quality_df = df['quality'].copy()
 quality_df[quality_df <= 5] = 0
 quality_df[quality_df > 5] = 1
 
-train = df.iloc[:1000]
-test = df.iloc[1000:]
-train_label = quality_df.iloc[:1000]
-test_label = quality_df.iloc[1000:]
+train = df.iloc[:1400]
+test = df.iloc[1400:]
+train_label = quality_df.iloc[:1400]
+test_label = quality_df.iloc[1400:]
 
-scaler = MinMaxScaler()
+print(train)
+scaler = StandardScaler()
 scaler.fit(train)
 train = scaler.transform(train)
 test = scaler.transform(test)
@@ -27,4 +28,5 @@ clf.fit(train, train_label)
 print(clf.score(test, test_label))
 
 test_predict = clf.predict(test)
-cm = confusion_matrix(test, test_predict)
+cm = confusion_matrix(test_label, test_predict)
+print(cm)
