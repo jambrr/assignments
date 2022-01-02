3bfe181964698de6a4f66fb797a990c293d326d5
jambrr <louis.weber99@gmail.com>
2022-01-02 01:50:17 +0100

    big data

--- cloud-based/assignment_4/assignment-louis_weber/src/main/java/com/example/demo/DemoApplication.java
+++ cloud-based/assignment_4/assignment-louis_weber/src/main/java/com/example/demo/DemoApplication.java
@@ -27,27 +27,45 @@ public class DemoApplication {
         return "Greetings from Spring Boot, Java version!";
     }
 
-    @RequestMapping("/get")
-    public String get() {
-        DBObject query = new BasicDBObject("_id", "1");
-        DBCursor cursor = dishes.find(query);
+    @RequestMapping("/get/{dishName}")
+    public String get(@PathVariable String dishName) {
+        System.out.println(dishName);
+        DBObject query = new BasicDBObject("_name", dishName);
+            DBCursor cursor = dishes.find(query);
 
-        return cursor.one().get("name").toString();
+        return cursor.one().toString();
     }
 
-    @RequestMapping("/order")
-    public String order() {
+    @RequestMapping("/order/{dishName}/{address}")
+    public String order(@PathVariable String dishName, @PathVariable String address) {
+        int rand = (int)(Math.random() * 1000);
+        
+        DBObject order = new BasicDBObject("_id", rand)
+            .append("name", dishName)
+            .append("address", address);
 
-        return "Order";
+        orders.insert(order);
+        return "Order of: "+dishName+" has been made and will be delivered to: "+address;
     }
+
+    @RequestMapping("/add/{dishName}/{description}/{category}/{price}")
+    public String add(@PathVariable String dishName, @PathVariable String description, @PathVariable String category, @PathVariable double price) {
+
+        DBObject dish = new BasicDBObject("_name", dishName)
+            .append("description", description)
+            .append("category", category)
+            .append("price", price);
+
+        dishes.insert(dish);
+        return "New dish has been added";
+    }
+
     public static void main(String[] args){
         try{
             mongoClient = new MongoClient("mongo_db", 27017);
             database = mongoClient.getDB("restaurant");
             dishes = database.getCollection("dishes");
             orders = database.getCollection("orders");
-            DBObject o = new BasicDBObject("_id", "1").append("name", "louis");
-            dishes.insert(o);
 
         }catch(Exception e){
             System.out.println("host not found");
