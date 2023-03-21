04db330d32ee84a40ed202fd2af17acdcb5a6ece
Jamber <louis.weber99@gmail.com>
2023-03-22 00:36:59 +0100

    Assignemnt 2 done

--- security/assignment_2.py
+++ security/assignment_2.py
@@ -1,31 +1,43 @@
 import math
 
 def addition(a, b, B):
+    a = a[::-1]
+    b = b[::-1]
     k = len(a)
     l = len(b)
     c = [0] * (k+1)
     carry = 0
 
     for i in range(l):
-        print(i)
         tmp = a[i] + b[i] + carry
         carry = tmp // B
         c[i] = tmp % B
 
-    print('test')
     for i in range(l, k):
-        print(i)
         tmp = a[i] + carry
         carry = tmp // B
         c[i] = tmp % B
 
     c[k] = carry
-    return c
+    return int("".join(map(str, c[::-1])))
 
-def fibonacci():
-    pass
+def fibonacci(n):
+    B = 10
+    u0 = 0
+    u1 = 1
+    a = [u0, u1]
+
+    for i in range(n-1):
+        x = addition([u0], [u1], B)
+        a.append(x)
+        u0 = u1
+        u1 = x
+
+    return a[-1]
 
 def multiplication(a, b, B):
+    a = a[::-1]
+    b = b[::-1]
     k = len(a)
     l = len(b)
     c = [0] * (k + l)
@@ -40,19 +52,33 @@ def multiplication(a, b, B):
 
         c[i+l] = carry
 
-    print(tmp)
-    print(carry)
+    return int("".join(map(str, c[::-1])))
 
-    return c
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        r = 1
+        for i in range(1, n+1):
+            r = multiplication([r], [i], B)
 
-def factorial():
-    pass
+        return r
 
 def expmod(a, b, m):
-    pass
+    c = 1
+    b = bin(b)[2:]
 
+    for i in b:
+        c = (c**2) % m 
+        if i == '1':
+            c = (c * a) % m
+
+    return c
 
 if __name__ == "__main__":
     B = 10
     print(addition([6, 4, 7], [8, 5], B))
     print(multiplication([3, 7], [8, 5], B))
+    print(fibonacci(101))
+    print(factorial(30))
+    #print(expmod(2342, 6762, 9343))
