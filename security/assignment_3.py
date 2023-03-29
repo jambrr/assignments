19d8904e51a44885c7d447ac2198418eb8e69fcb
jambrr <louis.weber99@gmail.com>
2023-03-29 23:05:49 +0200

    security

--- security/assignment_3.py
+++ security/assignment_3.py
@@ -1,29 +1,50 @@
 from sage.all import *
 from sage.misc.prandom import randrange
 
-def key_gen():
-    p = random_prime(1000)
-    q = random_prime(1000)
+#Fermat primality test
+def fermat(n, t):
+    for i in range(t+1):
+        a = randrange(2, n)
+        r = pow(a, n-1, n)
+
+        if r != 1:
+            return "composite"
+    return "prime"
+
+def prime_gen(k):
+    while True:
+        p = randint(2**(k-1), 2**k-1)
+
+        p = p | 2**(k-1)
+        p = p | 2**(k-2)
+
+        if is_prime(p):
+            return p
+
+def key_gen(k):
+    p = prime_gen(k/2)
+    q = prime_gen(k/2)
     
     n = p * q
     sigma = (p-1)*(q-1)
     
     e = -1
-    while e < 0
+    while e < 0:
         x = randrange(sigma)
         if gcd(x) == 1:
             e = x
-    
-    
 
     
+#Encrypt the message
+def encryption(e, m, n):
+    return (m**d) % n
 
-def encryption():
-    pass
-
-def decryption():
-    pass
+#Decrypt the message
+def decryption(d, c, n):
+    return (c**d) % n
 
 if __name__ == "__main__":
-    print(key_gen())
+    print(fermat(11, 7))
+    print(prime_gen(16))
+    #print(key_gen())
     
